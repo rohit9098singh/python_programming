0ea790ab20d02c102558c264943a8a20bdb091bd
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-12 00:48:50 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,11 @@
+a=10
+b=20
+sum=a+b
+sub=a-b
+mult=a*b
+div=b/a
+print(sum)
+print(sub)
+print(mult)
+print(div)
+print("hello world")
---
+++ second.py
@@ -0,0 +1,1 @@
+print("enter the size of an array you want to take ")
