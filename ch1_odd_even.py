42d0e1e500f403e7758226ed29b222e34c2d58cf
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-12 12:12:16 +0530

    odd_even

---
+++ ch1_odd_even.py
@@ -0,0 +1,6 @@
+value=int(input("enter a number to check wheather it is even or odd :"))
+if(value%2==0):
+    print(f"the enterd number is an even number: {value}")
+
+else:
+    print(f"the enterd number is an odd number: {value}")    
