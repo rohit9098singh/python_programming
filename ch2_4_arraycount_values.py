b37002084746c19d025491cf7107bc4a990273a6
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-12 23:45:38 +0530

    array count

---
+++ ch2_4_arraycount_values.py
@@ -0,0 +1,23 @@
+even = 0
+odd = 0
+
+try:
+    n = int(input("Enter the size of an array: "))
+except ValueError as e:
+    print(e)
+    print("Invalid data type entry. Please make a proper choice.")
+    exit(1)
+
+array = []
+
+for i in range(n):
+    value = int(input(f"Enter the value at index {i+1}: "))
+    array.append(value)  # Append the value to the list
+
+    if value % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print(f"The number of even numbers in your array is: {even}")
+print(f"The number of odd numbers in your array is: {odd}")
