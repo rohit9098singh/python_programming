c8a1f66a2f3fe7f6d4b574ebcf3cae7d6bbab4a1
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-12 11:40:57 +0530

    second python

--- second.py
+++ second.py
@@ -1,1 +1,20 @@
+n=int(input("enter the size of an array of your choice\n"))
+try:
+    array_size=n
+except ValueError as e:
+    print(e)
+    print("the value you have provided is not a valid parameter please enter integer value please")
+    exit(1) 
+print(f"You entered an array size of {array_size}") 
+
+
+my_array=[]
+
+for i in range(array_size):
+    values=input(f"enter the array element  {i+1} ")
+    my_array.append(values)
+
+#printing your array now 
+print("array",my_array)    
+
 
