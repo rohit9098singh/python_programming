908e15a6f7dad490c0425494a846d9bc6a0532b0
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-14 16:24:02 +0530

    palindrom

---
+++ ch3_3_string_palindrom.py
@@ -0,0 +1,16 @@
+
+def check_palindrom(input):
+    lower=input.lower()
+    reverse=input[::-1]
+    if lower==reverse:
+       # return True
+      print("the eneterd string  is a palindrom =",s)
+    else:
+        #return False
+         print("the enterd string is not a palindrom =",s)
+    
+s=input("enter the string of your choice :")
+check_palindrom(s)
+'''print("the eneterd string  is a palindrom =",s)
+else:
+    print("the enterd string is not a palindrom =",s)  ''' 
