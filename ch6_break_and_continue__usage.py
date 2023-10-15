a595274d04e9e581d0b813a5359ac71a654e6161
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-15 08:37:16 +0530

    break and continue

---
+++ ch6_break_and_continue__usage.py
@@ -0,0 +1,13 @@
+for i in range(1,11,1):
+    print(i,end=':')
+    if(i==4):
+     break
+    else:
+       print("meeting you was a best thing till now")
+
+for i in range(11):
+   if(i==11):
+      print("skipping(this itteration")
+      break
+   else:
+      print("5 x",i,"=",5*i)
