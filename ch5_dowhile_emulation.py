1facab8afb552305b85dc7e9752bc779c532c73a
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-15 08:21:44 +0530

    do while

---
+++ ch5_dowhile_emulation.py
@@ -0,0 +1,6 @@
+while True :
+    number=int(input("enter a postive number :"))
+    print(number)
+    if not(number>0):
+      print("you just enterd an negaitive value thus terminating the loop")
+      break    
