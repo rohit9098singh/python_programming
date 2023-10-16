7d9b49ff2fca5ed24f3d4509346b58e4df983c15
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-17 00:44:34 +0530

    doc string

---
+++ ch11_doc_strings.py
@@ -0,0 +1,9 @@
+
+def square(n):
+    '''............these are the string literals that appears right after the functiion defination........
+    take a number n and return the square of a n'''
+  
+    print(n*n)
+square(5)
+print(square.__doc__)   
+ 
