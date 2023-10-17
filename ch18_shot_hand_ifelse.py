8ce8febe97252ccbd2bdb41d7161e993c6b773b3
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-17 18:34:44 +0530

    short hand if else

---
+++ ch18_shot_hand_ifelse.py
@@ -0,0 +1,3 @@
+value1=int(input("enter the value that is to be stored in the first variabele :"))
+value2=int(input("enter the value that is to be stored in the second variable :"))
+print("value1") if value1<value2 else print("both the variables are =") if value1==value2 else print("value2") 
