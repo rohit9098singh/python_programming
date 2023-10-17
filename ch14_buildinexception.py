19e81b8adb2bbfe3f91b401e095e6d7cc9a283f7
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-17 10:46:41 +0530

    build in error

---
+++ ch14_buildinexception.py
@@ -0,0 +1,34 @@
+# 1. ZeroDivisionError
+try:
+    result = 10 / 0
+except ArithmeticError as e:
+    print(f"ZeroDivisionError: {e}")
+
+# 2. IndexError
+my_list = [1, 2, 3]
+try:
+    value = my_list[2]
+    print(value)
+except IndexError as e:
+    print(f"IndexError: {e}")
+
+
+# 4. TypeError
+try:
+    sum_result = '1' + 2
+except TypeError as e:
+    print(f"TypeError: {e}")
+
+# 5. ValueError
+try:
+    num = int('string')
+except ValueError as e:
+    print(f"ValueError: {e}")
+
+# 6. KeyError
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+try:
+    value = my_dict['d']
+except KeyError as e:
+    print(f"KeyError: {e}")
+
