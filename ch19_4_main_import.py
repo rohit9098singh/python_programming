3ca606667c653f17e3360581f79d48aab593b90a
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-18 16:27:14 +0530

    main in python

---
+++ ch19_4_main_import.py
@@ -0,0 +1,9 @@
+import CH19_3_MAIN_WITHMULTIFUNCTION
+result=CH19_3_MAIN_WITHMULTIFUNCTION.add(34,43)
+result2=CH19_3_MAIN_WITHMULTIFUNCTION.multiplication(34,4)
+result3=CH19_3_MAIN_WITHMULTIFUNCTION.divide(22,3)
+result4=CH19_3_MAIN_WITHMULTIFUNCTION.subtract(12,3)
+print(result)
+print(result2)
+print(result3)
+print(result4)
