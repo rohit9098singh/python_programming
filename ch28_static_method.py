f8a30dca9e4158685cca4f35dd65abba88633cba
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-10-21 16:33:33 +0530

    static methods

---
+++ ch28_static_method.py
@@ -0,0 +1,20 @@
+class mathoperations:
+    @staticmethod
+    def add(a,b):
+        return a+b
+    
+    @staticmethod
+    def mult(a,b):
+        return a*b
+    
+    @staticmethod
+    def sub(a,b):
+        return a-b
+    
+    @staticmethod
+    def divide(a,b):
+        return a/b
+print("addition =",mathoperations.add(3,4))
+print("addition =",mathoperations.sub(8,4))
+print("addition =",mathoperations.mult(30,4))
+print("addition =",mathoperations.divide(30,10))
