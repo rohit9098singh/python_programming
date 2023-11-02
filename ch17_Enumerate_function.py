81b7011ee58a80fb073f3a3c06712ae5d017216a
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-11-02 23:36:56 +0530

    list

--- ch17_Enumerate_function.py
+++ ch17_Enumerate_function.py
@@ -3,6 +3,7 @@ for i,j in enumerate(new_list):
     print(f"the element present at index: {i} is {j}")
 
 print("========using another loop and printing in different way==========")
-for i,j in enumerate(new_list,start=1):#end function is not supported in enummerator we will have to explicitly use if to itterate over the list as per our start and end
+for i,j in enumerate(new_list,start=1):#end function is not supported in enummerator we will have to explicitly use 
+                                       #if to itterate over the list as per our start and end
      if 1 <= i <= 2:
        print(f"the element present at the index: {i} is {j}")    
