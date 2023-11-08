19538c3531608138d22ca0340281544a8e1b4c9a
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-11-08 09:03:30 +0530

    reduce

--- ch23_3_reduce.py
+++ ch23_3_reduce.py
@@ -9,3 +9,13 @@ print(result)
 
 
 # reduce kya karega ke sabse pehle 1+2=3,then 3+3=6, then 6+4=10, then 10+5=15.
+'''from functools import reduce
+import operator
+
+# Your number list
+numbers = [1, 2, 3, 4, 5]
+
+# Using reduce with operator.add to get the sum of the numbers
+result = reduce(operator.add, numbers)
+print(result)
+'''
